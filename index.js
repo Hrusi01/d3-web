d30446ddb12b9990ad313020eae29ef9e45769a5
Roshan Dash <roshan1337d@gmail.com>
2022-11-08 00:26:32 +0530

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+const pageLoaded = () => {
+    // Animate on scroll
+    AOS.init(
+        {
+            duration: 800,
+            once: true
+        }
+    );
+
+    // Remove loader
+    const loader = document.getElementById('loader_block');
+    loader.style.opacity = '0';
+    setTimeout(() => loader.style.display = 'none', 300);
+
+    window.removeEventListener('load', pageLoaded);
+}
+window.addEventListener('load', pageLoaded);
